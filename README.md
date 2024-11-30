7a93f262d0769772898833bc837230879453a821
Alexandre <alexandreh133@gmail.com>
2024-11-30 19:31:27 -0300

    Define the base folder structure

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# cs_fd_mleng
+## CS_FD_MLENG - A simple case
+
+**Description**
+
+This repo is a case of how to productionize a machine learning model for fraud detection.
+
+---
+
+**How to run**
+
+For now **the case is in development**. The sugestion is to build the docker image and run the container.
+
+```bash
+docker build -t project_tag .
+```
+
+After that, you can run the container with the following command:
+
+```bash
+docker run -it -v $(pwd)/models:/app/models image_name 
+```
---
+++ src/train/train.py
@@ -0,0 +1,49 @@
+import os
+import pickle
+
+import pandas as pd
+import yaml
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import f1_score
+from sklearn.model_selection import train_test_split
+
+## Load params and data
+
+CONFIG_PATH = "."
+
+
+def load_config(config_name):
+    with open(os.path.join(CONFIG_PATH, config_name)) as file:
+        config = yaml.safe_load(file)
+
+    return config
+
+
+config = load_config("train_params.yaml")
+
+df = pd.read_csv(f'../../data/train_{config["state"]}.csv')
+
+## Split
+
+X = df.drop(["is_fraud", "state"], axis=1)
+y = df["is_fraud"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+## Model
+
+model = LogisticRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+## Evaluation
+
+print("F1:", f1_score(y_test, y_pred))
+
+## Persist
+
+
+def persist_model(model, state):
+    pickle.dump(model, open(f'{config["models_dir"]}/model_{state}2.pkl', "wb"))
+
+
+persist_model(model, config["state"])
